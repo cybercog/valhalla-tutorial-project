292fcb4609c8400db0b9487146bb9e5fbb8eba5f
Mohamed Said <themohamedsaid@gmail.com>
2016-03-27 15:05:10 +0000

    Initial

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Authenticate a third-party application.
+     *
+     * @param Request $request
+     */
+    public function authenticateApp(Request $request)
+    {
+
+    }
+
+    /**
+     * Authenticate a user from a third-party application.
+     *
+     * @param Request $request
+     */
+    public function authenticateUser(Request $request)
+    {
+
+    }
+
+    /**
+     * Log a user out from a third-party application.
+     *
+     * @param Request $request
+     */
+    public function logoutUser(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+});
+
+
+$router->group(['prefix' => 'api/v1'], function ($router) {
+    // Applications Authentication...
+        $router->post('/auth/app', 'Api\AuthController@authenticateApp');
+
+    // Users Authentication...
+    $router->post('/auth/user', 'Api\AuthController@authenticateUser');
+    $router->post('/auth/user/logout', 'Api\AuthController@logoutUser');
+
+    // Testing routes...
+    $router->get('/application-data', 'Api\ApplicationController@data');
+    $router->get('/user-data', 'Api\UserController@data');
+});
